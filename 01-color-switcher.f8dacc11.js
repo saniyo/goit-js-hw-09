fa3ffb7b81e7df17edd1234189be1d21f1a0dfb8
saniyo <saniyo.ua@gmail.com>
2022-12-09 08:52:28 +0000

    Deploying to gh-pages from @ saniyo/goit-js-hw-09@11f5bc555b9b055feed51252f005be116a571b30 🚀

---
+++ 01-color-switcher.f8dacc11.js
@@ -0,0 +1,2 @@
+!function(){var t={btnStart:document.querySelector("button[data-start]"),btnStop:document.querySelector("button[data-stop]"),body:document.body},n=null;function o(){t.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}t.btnStart.addEventListener("click",(function(e){t.btnStart.disabled=!0,t.btnStop.disabled=!1,n=setInterval(o,1e3)})),t.btnStop.addEventListener("click",(function(o){t.btnStart.disabled=!1,t.btnStop.disabled=!0,clearInterval(n),n=0})),t.btnStop.disabled=!0}();
+//# sourceMappingURL=01-color-switcher.f8dacc11.js.map
